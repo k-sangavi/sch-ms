438365d70f5b1750f689950cbba92368eb4cabb8
Sangavi Karthi <sangavidev24@gmail.com>
2025-07-11 22:54:07 +0530

    app files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Router from "./components/router/Router";
+
+function App() {
+  return (
+    <>
+      <h2>School Management System</h2>
+      <Router />
+    </>
+  );
+}
+
+export default App;
