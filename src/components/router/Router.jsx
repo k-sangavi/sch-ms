17ae3db758976bb277bc83e6d0064dcb6dfe1b13
Sangavi Karthi <sangavidev24@gmail.com>
2025-07-12 22:51:42 +0530

    router file updated

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Student from "./../student/Student";
 import Teacher from "./../teacher/Teacher";
-import About from "../About/About";
-import AddStudent from "../AddStudent/AddStudent";
+import About from "../about/About";
+import AddStudent from "./../AddStudent/AddStudent";
 import AddTeacher from "./../AddTeacher/AddTeacher";
+import EditStudent from "./../EditStudent/EditStudent";
+import EditTeacher from "./../EditTeacher/EditTeacher";
 
 const Router = () => {
   const [data, setData] = useState([]);
@@ -19,6 +21,8 @@ const Router = () => {
           <Route path="/about" element={<About />} />
           <Route path="/addstudent" element={<AddStudent />} />
           <Route path="/addteacher" element={<AddTeacher />} />
+          <Route path="/editstudent" element={<EditStudent data={data} />} />
+          <Route path="/editteacher" element={<EditTeacher editTeacherData={editTeacherData} />} />
         </Routes>
       </BrowserRouter>
     </div>
